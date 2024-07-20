3989dca1bb0d65ab29ff55ddd1d479d9074ccac8
Caen De Silva <caen@desilva.se>
2024-07-20 11:50:49 +0200

    Create wget.php

---
+++ wget.php
@@ -0,0 +1,10 @@
+<?php
+
+// PGET = Minimal cross-platform WGET implementation in PHP
+
+// Get URL from first argument 
+$url = $argv[1];
+
+$contents = file_get_contents($url);
+
+file_put_contents(basename($url), $contents);
